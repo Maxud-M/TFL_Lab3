7c4c72eb5d2f008eb3b07e00648211aa94f11cd6
Maxud-M <maksud.maksudov.00@mail.ru>
2021-11-16 13:57:12 +0300

    Scripted auto-commit on change (2021-11-16 13:57:12) by gitwatch.sh

--- src/main/java/PDA.java
+++ src/main/java/PDA.java
@@ -7,6 +7,7 @@ public class PDA {
     String stackBottom;
 
     PDA(ArrayList<String> rules) {
+            
 
         for(int i = 1; i < rules.size(); ++i) {
             this.rules.add(RuleReader.readRule(rules.get(i)));
