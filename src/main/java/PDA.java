73c7011ed26a89a1466e42678e7c5b3395dac93a
Maxud-M <maksud.maksudov.00@mail.ru>
2021-11-16 12:49:32 +0300

    Scripted auto-commit on change (2021-11-16 12:49:32) by gitwatch.sh

--- src/main/java/PDA.java
+++ src/main/java/PDA.java
@@ -1,9 +1,19 @@
 public class PDA {
-    private class Rule{ //(state1, letter, )
-        String state1;
-        String state2;
-        char letter;
-        String stackS1;
-        String stackS2;
+    private class Rule{ //<state1, letter, stackS1> -> <state2, stackS2>
+        public String state1;
+        public String state2;
+        public char letter;
+        public String stackS1;
+        public String stackS2;
+
+        Rule(String state1, String state2, char letter, String stackS1, String stackS2) {
+            this.state1 = state1;
+            this.state2 = state2;
+            this.stackS1 = stackS1;
+            this.stackS2 = stackS2;
+            this.letter = letter;
+        }
+
+        
     }
 }
