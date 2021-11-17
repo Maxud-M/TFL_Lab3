54e149c45721096775f537610b6b1aaf974824d2
Maxud-M <maksud.maksudov.00@mail.ru>
2021-11-17 21:39:47 +0300

    Scripted auto-commit on change (2021-11-17 21:39:47) by gitwatch.sh

--- src/main/java/CFG.java
+++ src/main/java/CFG.java
@@ -1,2 +1,30 @@
+import java.util.ArrayList;
+
 public class CFG {
+
+    ArrayList<Rule> rules;
+    Character startSymbol 
+
+
+    public static class Rule{
+        private Character leftPart;
+        private String rightPart;
+
+        public Character getLeftPart() {
+            return leftPart;
+        }
+
+        public String getRightPart() {
+            return rightPart;
+        }
+
+        Rule(Character leftPart, String rightPart) {
+            this.leftPart = leftPart;
+            this.rightPart = rightPart;
+        }
+
+
+
+    }
+
 }
